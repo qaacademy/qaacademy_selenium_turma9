7431d64c33af0773826fbb33252ce027543c9757
Flavio Dias <flavio.goncalvesdias@gmail.com>
2024-03-23 15:45:03 -0300

    Exercicios Windows

--- qaacademy_selenium_turma09/src/test/java/ExerciciosWindow.java
+++ qaacademy_selenium_turma09/src/test/java/ExerciciosWindow.java
@@ -1,4 +1,6 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,15 +8,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class ExerciciosTab {
+public class ExerciciosWindow {
+    WebDriver driver;
 
-    @Test
-    public void testeTabsWindows() {
-
-        WebDriver driver = new ChromeDriver();
+    @Before
+    public void before() {
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://demo.automationtesting.in/Windows.html");
         driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[2]/div[2]/button[1]/p")).click();
+    }
+
+    @Test
+    public void testeTabsWindows() {
 
         driver.findElement(By.xpath("//body/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]/button[1]")).click();
         //Mapeamento das tabs abertas
@@ -24,6 +30,23 @@ public class ExerciciosTab {
         String textNovaTab = "Selenium automates browsers. That's it!";
         String paginaTotal = driver.getPageSource();
         Assert.assertTrue(paginaTotal.contains(textNovaTab));
+    }
+
+    @Test
+    public void testeNewWindow() {
+        driver.findElement(By.xpath("//a[contains(text(),'Open New Seperate Windows')]")).click();
+        driver.findElement(By.xpath("//body/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/button[1]")).click();
+        //Mapeamento das tabs abertas
+        Object[] janelas = driver.getWindowHandles().toArray();
+        //Troca a tab, passando a referencia
+        driver.switchTo().window(janelas[1].toString());
+        String textNovaTab = "Selenium automates browsers. That's it!";
+        String paginaHtml = driver.getPageSource();
+        Assert.assertTrue(paginaHtml.contains(textNovaTab));
+    }
+
+    @After
+    public void after(){
         driver.quit();
     }
 }
